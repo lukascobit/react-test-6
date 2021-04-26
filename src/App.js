ce5cd119e0f88ffb5b86c63a5e65f8a3e5505a19
Lukas <lukyn.odehnal333@gmail.com>
2021-04-26 16:35:11 +0200

    added height

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import './App.css';
 function App() {
 
   const [windowWidth,setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight,setWindowHeight] = useState(window.innerHeight);
 
   const Resize = ()=>{
-    setWindowWidth(window.innerWidth)
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
   }
 
 
   useEffect(()=>{
-    window.addEventListener('resize', Resize)
+    window.addEventListener('resize', Resize);
+
+    /*return()=>{
+      window.addEventListener('resize', Resize);
+    }*/
   },[])
 
   return (
     <div>
-
-      
       <h1>Window width:  {windowWidth}</h1>
+      <h1>Window height:  {windowHeight}</h1>
     </div>
   );
 }
