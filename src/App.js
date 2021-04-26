171257f5886453f7e362a13bbe94957515a86759
Lukas <lukyn.odehnal333@gmail.com>
2021-04-26 14:48:23 +0200

    adding more

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React,{ useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [count,setCount] = useState(4);
+
+  function Plus(){
+    setCount(count+1)
+  }
+  function Minus(){
+    setCount(count-1)
+  }
+
   return (
     <div>
-      
+      <button onClick={Plus}>+</button>
+      <h1>{count}</h1>
+      <button onClick={Minus}>-</button>
+
     </div>
   );
 }
