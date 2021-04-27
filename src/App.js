563dee6a233a208d1f2328acd21869722c264101
Lukas <lukyn.odehnal333@gmail.com>
2021-04-27 13:23:39 +0200

    another commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import React,{ useState, useEffect, useRef, useReducer } from 'react'
+import React,{ useReducer } from 'react'
 import './App.css';
 
-function App() {
-  const [name,setName] = useState('')
-  const [windowWidth,setWindowWidth] = useState(window.innerWidth);
-  const [windowHeight,setWindowHeight] = useState(window.innerHeight);
-  const PrevName = useRef('');
-  const [counter,SetCounter] = useState(0)
 
+function reducer(state,action){
+  switch (action.type){
+    case 'increment':
+      return {count: state.count+1}
+    case 'decrement':
+      return {count: state.count-1}
+    default:
+      return state
+  }
+}
+
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { count:0 })
   function up(){
-    SetCounter(prevCount => prevCount+1)
+    dispatch({type: 'increment'})
   }
   function down(){
-    SetCounter(prevCount => prevCount-1)
+    dispatch({type: 'decrement'})
   }
-
-  const Resize = ()=>{
-    setWindowWidth(window.innerWidth);
-    setWindowHeight(window.innerHeight);
-  }
-  useEffect(()=>{
-    window.addEventListener('resize', Resize);
-    PrevName.current= name
-  },[name])
-
-
+  
   return (
     <div>
-      <h1>Window width:  {windowWidth}</h1>
-      <h1>Window height:  {windowHeight}</h1>
+
       <div>
-        <input value={name} onChange={e => setName(e.target.value)}></input>
-        <h2>new: {name}</h2>
-        <h2>old: {PrevName.current}</h2>
-      </div>
-      <div>
-        <h1>{counter}</h1>
+        <h1>{state.count}</h1>
         <button onClick={up}>+</button>
         <button onClick={down}>-</button>
       </div>
-    </div>
-    
+    </div>    
   );
 }
 
